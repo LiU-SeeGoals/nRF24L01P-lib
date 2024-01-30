1ea253d4fb257781d773d226e2261a8a3692fea4
rwn <contact@0x57.me>
2024-01-30 22:15:56 +0100

    added defines

--- nrf_helper_defines.h
+++ nrf_helper_defines.h
@@ -11,9 +11,11 @@
  * See page 54 in datasheet for NRF.
  */
 //!@{
-#define STATUS_BIT_RX_DR          6
-#define FEATURE_BIT_EN_ACK_PAY    1
-#define FEATURE_BIT_EN_DPL        2
+#define STATUS_MAX_RT         4
+#define STATUS_TX_DS          5
+#define STATUS_RX_DR          6
+#define FEATURE_EN_ACK_PAY    1
+#define FEATURE_EN_DPL        2
 //!@}
 
 /**
@@ -22,8 +24,8 @@
  * Are used to mask out wanted bits from registers in the device. Makes code easier to read.
  */
 //!@{
-#define STATUS_MASK_BIT_RX_DR      0x40       /**< Data ready in RX FIFO interrupt. */
-#define STATUS_MASK_BIT_TX_DS      0x20
-#define STATUS_MASK_BIT_MAX_RT     0x10
-#define STATUS_MASK_BITS_RX_P_NO   0x0E       /**< Data pipe number for payload available for readin from RX_FIFO. */
+#define STATUS_MASK_RX_DR      0x40       /**< Data ready in RX FIFO interrupt. */
+#define STATUS_MASK_TX_DS      0x20
+#define STATUS_MASK_MAX_RT     0x10
+#define STATUS_MASK_RX_P_NO    0x0E       /**< Data pipe number for payload available for readin from RX_FIFO. */
 //!@}
